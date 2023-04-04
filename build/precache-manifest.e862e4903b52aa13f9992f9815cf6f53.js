c79760bae702f65e52b5c53f6f99a7d7563c27f7
username <vasanthkumars2025@gmail.com>
2023-04-04 20:15:55 +0530

    added build

---
+++ build/precache-manifest.e862e4903b52aa13f9992f9815cf6f53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ded06e40795955f5ad42ad67234ca78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d327c62e5597d30482f1",
+    "url": "/static/css/main.9d1102b6.chunk.css"
+  },
+  {
+    "revision": "d87fb51bc7bdbe47a59e",
+    "url": "/static/js/2.b08a6f8c.chunk.js"
+  },
+  {
+    "revision": "b8ea6dc0cc15a5bf62ab0bb8486d0746",
+    "url": "/static/js/2.b08a6f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d327c62e5597d30482f1",
+    "url": "/static/js/main.9b0ad81d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9b0ad81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
