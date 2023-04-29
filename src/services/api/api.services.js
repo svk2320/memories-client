19d5c1e413ddbace32b35ffd516503bdd33c00a6
Vasanth Kumar S <112621092+vasanth2320@users.noreply.github.com>
2023-04-29 11:11:39 +0000

    changed url in api.services.js

--- src/services/api/api.services.js
+++ src/services/api/api.services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-app-production-dbdc.up.railway.app' });
+const API = axios.create({ baseURL: 'https://memories-server-production-c2e0.up.railway.app/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
